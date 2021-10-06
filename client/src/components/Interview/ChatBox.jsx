015e05eb8fe35d0c395823a82b980a57d5aa82b2
jerrenss <gerrenseow@gmail.com>
2021-10-06 23:22:19 +0800

    css fixes

--- client/src/components/Interview/ChatBox.jsx
+++ client/src/components/Interview/ChatBox.jsx
@@ -23,12 +23,12 @@ const useStyles = makeStyles((theme) => ({
     padding: 8,
     overflow: 'scroll',
     display: 'flex',
-    flexDirection: 'column',
+    flexDirection: 'column-reverse',
   },
   userMessage: {
     background: theme.palette.background.tertiary,
     maxWidth: '45%',
-    marginBottom: 8,
+    marginBottom: 4,
     padding: 8,
     borderRadius: theme.shape.borderRadius,
     alignSelf: 'end',
@@ -53,6 +53,12 @@ const useStyles = makeStyles((theme) => ({
     padding: 4,
     margin: 'auto',
   },
+  textBox: {
+    '& .MuiOutlinedInput-input': {
+      padding: '10px 16px',
+      fontSize: 16,
+    },
+  },
   btnWrapper: {
     margin: 'auto',
     textAlign: 'center',
@@ -65,24 +71,9 @@ const ChatBox = () => {
 
   const MOCK_CHAT = [
     {
-      uid: 'user1',
-      message: 'Hello there!',
-      timestamp: '2020-10-05 14:01:10-08',
-    },
-    {
-      uid: 'user1',
-      message: 'How are you doing?',
-      timestamp: '2020-10-05 14:02:10-08',
-    },
-    {
       uid: 'user2',
-      message: 'Nice to meet you!',
-      timestamp: '2020-10-05 14:03:10-08',
-    },
-    {
-      uid: 'user2',
-      message: 'I am doing good, just not really a morning person HAHA',
-      timestamp: '2020-10-05 14:04:10-08',
+      message: 'Sure!',
+      timestamp: '2020-10-05 14:06:10-08',
     },
     {
       uid: 'user1',
@@ -91,8 +82,23 @@ const ChatBox = () => {
     },
     {
       uid: 'user2',
-      message: 'Sure!',
-      timestamp: '2020-10-05 14:06:10-08',
+      message: 'I am doing good, just not really a morning person HAHA',
+      timestamp: '2020-10-05 14:04:10-08',
+    },
+    {
+      uid: 'user2',
+      message: 'Nice to meet you!',
+      timestamp: '2020-10-05 14:03:10-08',
+    },
+    {
+      uid: 'user1',
+      message: 'How are you doing?',
+      timestamp: '2020-10-05 14:02:10-08',
+    },
+    {
+      uid: 'user1',
+      message: 'Hello there!',
+      timestamp: '2020-10-05 14:01:10-08',
     },
   ]
   const [chat, setChat] = useState(MOCK_CHAT)
@@ -109,8 +115,7 @@ const ChatBox = () => {
       message: input,
       timestamp: new Date().getTime().toString(),
     }
-    const newChat = [...chat, newMessage]
-    setChat(newChat)
+    setChat([newMessage, ...chat])
     setInput('')
   }
 
@@ -137,6 +142,7 @@ const ChatBox = () => {
         <form className={classes.formWrapper}>
           <Grid item xs={9} className={classes.inputWrapper}>
             <TextField
+              className={classes.textBox}
               variant="outlined"
               placeholder="Chat with your buddy here"
               fullWidth

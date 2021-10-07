a060d6900009aeb8f8cf294b1346817a9b1bc780
jerrenss <gerrenseow@gmail.com>
2021-10-08 01:25:02 +0800

    added localstorage interaction

--- client/src/storage/index.js
+++ client/src/storage/index.js
@@ -5,3 +5,7 @@ export const saveStorage = (key, value) => {
 export const fetchStorage = (key) => {
   return localStorage.getItem(key)
 }
+
+export const clearStorage = (key) => {
+  localStorage.removeItem(key)
+}

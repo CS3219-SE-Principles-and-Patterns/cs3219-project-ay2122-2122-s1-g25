15f728910daa45b2a18886a4371e2c825d1fcad9
jerrenss <gerrenseow@gmail.com>
2021-10-07 21:32:25 +0800

    setup cloud run deployment

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -2,10 +2,24 @@ const { Pool } = require('pg');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const connectionString = process.env.DB_CONNECTION_STRING
-
-const pool = new Pool({
-    connectionString
-});
+const config = {
+    DEV: {
+        host: 'database',
+        port: 5432,
+        user: 'upskill_user',
+        password: 'upskill_pw',
+        database: 'upskill_db',
+    },
+    PROD: {
+        host: '/cloudsql/upskill-prod:asia-southeast1:upskill-db-prod',
+        user: 'postgres',
+        password: 'upskill-db-prod-pw',
+        database: 'upskill-db',
+        socketPath:
+        '/cloudsql/upskill-prod:asia-southeast1:upskill-db-prod',
+    },
+}
+  
+const pool = new Pool(config[process.env.NODE_ENV])
 
 module.exports = pool;

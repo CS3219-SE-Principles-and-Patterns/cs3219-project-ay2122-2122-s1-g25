6c73663993431b5e589f51b0e4c9091f70594af4
jerrenss <gerrenseow@gmail.com>
2021-10-09 16:55:33 +0800

    schema updates

--- server/migrations/001_schema.sql
+++ server/migrations/001_schema.sql
@@ -1,6 +1,6 @@
 SET TIMEZONE = 'Asia/Singapore';
 
-DROP TABLE IF EXISTS Users, InterviewSessions, Questions, Rotations, Feedback, UserMatching cascade;
+DROP TABLE IF EXISTS Users, InterviewSessions, Questions, Rotations, Feedbacks, UserMatching;
 
 CREATE TABLE IF NOT EXISTS Users (
    	userId TEXT PRIMARY KEY,
@@ -11,14 +11,19 @@ CREATE TABLE IF NOT EXISTS Users (
 );
 
 CREATE TABLE IF NOT EXISTS InterviewSessions (
-    iSessionId INTEGER PRIMARY KEY,
-    timeslot TIMESTAMP
+    iSessionId SERIAL PRIMARY KEY,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    user0 TEXT NOT NULL REFERENCES Users,
+    user1 TEXT NOT NULL REFERENCES Users,
+    rotation SMALLINT NOT NULL,
+    complete BOOLEAN NOT NULL,
+    difficulty SMALLINT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Questions (
     questionId SERIAL PRIMARY KEY,
-    category TEXT,
-    difficulty SMALLINT, --1 being easy, 3 being hard
+    category TEXT NOT NULL,
+    difficulty SMALLINT NOT NULL, --1 being easy, 3 being hard
     description TEXT NOT NULL,
     input TEXT[] NOT NULL,
     output TEXT[] NOT NULL,
@@ -26,28 +31,20 @@ CREATE TABLE IF NOT EXISTS Questions (
 );
 
 CREATE TABLE IF NOT EXISTS Rotations (
-    interviewerId TEXT,
-    intervieweeId TEXT,
-    questionId INTEGER,
-    iSessionId INTEGER,
-    attempt TEXT,
-    PRIMARY KEY (interviewerId, intervieweeId, iSessionId),
-    FOREIGN KEY(interviewerId) REFERENCES Users,
-    FOREIGN KEY(intervieweeId) REFERENCES Users,
-    FOREIGN KEY(questionId) REFERENCES Questions,
-    FOREIGN KEY(iSessionId) REFERENCES InterviewSessions
+    iSessionId INTEGER NOT NULL REFERENCES InterviewSessions,
+    rotation SMALLINT NOT NULL,
+    questionId INTEGER NOT NULL REFERENCES Questions,
+    attempt TEXT NOT NULL DEFAULT '',
+    PRIMARY KEY(iSessionId, rotation)
 );
 
-CREATE TABLE IF NOT EXISTS Feedback (
+CREATE TABLE IF NOT EXISTS Feedbacks (
   	feedbackId SERIAL PRIMARY KEY,
-    giverId TEXT, 
-    receiverId TEXT, 
-    iSessionId INTEGER,
+    giverId TEXT NOT NULL REFERENCES Users, 
+    receiverId TEXT NOT NULL REFERENCES Users, 
+    iSessionId INTEGER NOT NULL REFERENCES InterviewSessions,
     rating SMALLINT NOT NULL,
-    comment TEXT,
-    FOREIGN KEY(giverId) REFERENCES Users,
-    FOREIGN KEY(receiverId) REFERENCES Users,
-    FOREIGN KEY(iSessionId) REFERENCES InterviewSessions
+    comment TEXT NOT NULL DEFAULT '',
 );
 
 CREATE TABLE IF NOT EXISTS UserMatching (

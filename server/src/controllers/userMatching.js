65ae6469e0ed62b1e824e3795466bb7d3a7f6c1c
unknown <pangkimjin2@gmail.com>
2021-10-11 17:40:29 +0800

    added logic to match users

--- server/src/controllers/userMatching.js
+++ server/src/controllers/userMatching.js
@@ -11,7 +11,19 @@ exports.getUserMatching = async (req, res) => {
     }
 }
 
+exports.getAllAvailableUserMatching = async (req, res) => {
+    try {
+        const userMatching = new UserMatching();
+        const availableUserMatchings = await userMatching.getAllAvailableUserMatching();
+        res.status(200).json(availableUserMatchings.rows);
+    } catch (err) {
+        res.status(400).json({ errMsg: err });
+    }
+}
+
 exports.createUserMatching = async (req, res) => {
+    // try to return interview session id instead
+    // call another function if can find a match and maybe smaller userId create the interview session?
     const { userId, difficulty } = req.body;
     try {
         const startedMatchingAt = new Date();
@@ -19,7 +31,42 @@ exports.createUserMatching = async (req, res) => {
 
         const userMatching = new UserMatching();
         const newUserMatching = await userMatching.createUserMatching(userId, startedMatchingAt, difficulty, matchId);
-        res.status(200).json(newUserMatching.rows);
+
+        // loop through for 30 seconds to find a match
+        let tries = 0;
+        while (tries < 7) {
+            const userMatching = new UserMatching();
+            let currentUserMatching = await userMatching.getUserMatching(userId);
+            if (JSON.parse(JSON.stringify(currentUserMatching.rows[0])).matchid) {
+                // user has been chosen by another user
+                res.status(200).json(currentUserMatching.rows);
+                break;
+            } else if (tries == 6) {
+                // 30s timeout reached
+                const deletedUserMatching = await userMatching.deleteUserMatching(userId);
+                res.status(200).json(deletedUserMatching.rows)
+                break;
+            } else {
+                // user searches for an available user
+                const userMatch = new UserMatching();
+                // gets all UserMatchings with matchId = null (excluding currentUserMatching)
+                let availableUserMatchings = await userMatch.getAllAvailableUserMatching(userId);
+                if (availableUserMatchings.rows.length > 0) {
+                    // get match with the first (oldest) entry
+                    let difficulty = JSON.parse(JSON.stringify(availableUserMatchings.rows[0])).difficulty;
+                    let matchedId = JSON.parse(JSON.stringify(availableUserMatchings.rows[0])).userid;
+                    currentUserMatching = await userMatch.updateUserMatching(userId, matchedId);
+                    availableUserMatchings = await userMatch.updateUserMatching(matchedId, userId);
+
+                    let newInterviewSession = await initialiseInterviewSession(userId, matchedId, difficulty);
+
+                    res.status(200).json(currentUserMatching.rows);
+                    break;
+                }
+                await sleep(5000);
+                tries++;
+            }
+        }
     } catch (err) {
         res.status(400).json({ errMsg: err });
     }
@@ -46,4 +93,10 @@ exports.deleteUserMatching = async (req, res) => {
     } catch (err) {
         res.status(400).json({ errMsg: err })
     }
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
 }
--- server/src/models/userMatching.js
+++ server/src/models/userMatching.js
@@ -5,6 +5,10 @@ class UserMatching {
         return pool.query("SELECT * FROM UserMatching WHERE userId = $1", [userId])
     }
 
+    getAllAvailableUserMatching(userId) {
+        return pool.query("SELECT * FROM UserMatching WHERE matchId is null AND userId != $1", [userId])
+    }
+
     createUserMatching(userId, startedMatchingAt, difficulty, matchId) {
         return pool.query("INSERT INTO UserMatching(userId, startedMatchingAt, difficulty, matchId) VALUES($1, $2, $3, $4) RETURNING *", [userId, startedMatchingAt, difficulty, matchId])
     }

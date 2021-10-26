7a1c43be4313172f6693cf092861f4f349ebf85c
Yuanyu97 <e0322803@u.nus.edu>
2021-10-27 00:02:49 +0800

    add model testing for questions

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -16,10 +16,17 @@ const config = {
         password: 'upskill-db-prod-pw',
         database: 'upskill-db',
         socketPath:
-        '/cloudsql/upskill-prod:asia-southeast1:upskill-db-prod',
+            '/cloudsql/upskill-prod:asia-southeast1:upskill-db-prod',
     },
+    TEST: {
+        host: 'localhost',
+        port: 5433,
+        user: 'upskill_user',
+        password: 'upskill_pw',
+        database: 'upskill_db',
+    }
 }
-  
+
 const pool = new Pool(config[process.env.NODE_ENV])
 
 module.exports = pool;

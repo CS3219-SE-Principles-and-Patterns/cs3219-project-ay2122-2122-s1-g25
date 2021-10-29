129904c48a22cf16692df86b2191e92bc553c6e5
jerrenss <gerrenseow@gmail.com>
2021-10-29 16:45:12 +0800

    inject JWT token into client requests

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,8 +1,27 @@
 import axios from 'axios'
+import firebase from './firebase'
+
+const injectJWTToken = (config) => {
+  const user = firebase.auth().currentUser
+  if (user) {
+    user
+      .getIdToken()
+      .then((token) => {
+        config.headers.Authorization = `Bearer ${token}`
+        return config
+      })
+      .catch((err) => err)
+  }
+  return config
+}
 
 const instance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api`,
   timeout: 5000,
+})
+
+instance.interceptors.request.use((config) => {
+  return injectJWTToken(config)
 })
 
 export const GETRequest = (url, params = {}) => {
@@ -25,6 +44,10 @@ export const DELETERequest = (url, params = {}) => {
 const longInstance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api`,
   timeout: 40000,
+})
+
+longInstance.interceptors.request.use((config) => {
+  return injectJWTToken(config)
 })
 
 export const POSTLongRequest = (url, data = {}) => {

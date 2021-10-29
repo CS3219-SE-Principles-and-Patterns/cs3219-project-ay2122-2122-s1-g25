f04a6c4c67fb9f40dba21da39b8da8738572869f
unknown <pangkimjin2@gmail.com>
2021-10-30 00:45:58 +0800

    set credentials to be requested from env variables

--- server/src/index.js
+++ server/src/index.js
@@ -40,10 +40,13 @@ socketDriver(server)
 
 // JWT imports
 const admin = require('firebase-admin')
-const serviceAccount = require("../serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  }),
   databaseURL: ""
 });
 
